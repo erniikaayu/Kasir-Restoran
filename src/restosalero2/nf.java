f323887df4d2c2079f06853362f49c0cc608abca
NOVA <nova.22048@mhs.unesa.ac.id>
2024-06-08 22:41:32 +0700

    first commit

---
+++ src/restosalero2/nf.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restosalero2;
+
+/**
+ *
+ * @author USER
+ */
+class nf {
+     static String Format(int sub_total) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+     }
+
+    static String format(int hasil) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
